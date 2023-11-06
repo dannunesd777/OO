624b6d64c9c164e58282e78e8b1ac2f8832d54a5
dannunesd777 <211062910@aluno.unb.br>
2023-11-06 13:50:03 -0300

    subindo

--- entregas/entrega2/circulo.py
+++ entregas/entrega2/circulo.py
@@ -1,16 +1,16 @@
 import math
 
-# Função para calcular a área e o perímetro de um círculo
+# Calcular a área e o perímetro do círculo
 def calcular_circulo():
     pi = 3.1415926
     raio = float(input("Digite o raio do círculo: "))
 
-    # Função para calcular a área do círculo
+    # Calcular a área do círculo
     def area_circulo():
         area = pi * (raio ** 2)
         return area
 
-    # Função para calcular o perímetro do círculo
+    # Calcular o perímetro do círculo
     def perimetro_circulo():
         perimetro = 2 * pi * raio
         return perimetro
--- entregas/entrega2/cubo.py
+++ entregas/entrega2/cubo.py
@@ -1,13 +1,13 @@
-# Função para calcular a área e o volume de um cubo
+# Calcular a área e o volume do cubo
 def calcular_cubo():
     lado = float(input("Digite o valor do lado do cubo: "))
 
-    # Função para calcular a área do cubo
+    # Calcular a área do cubo
     def area_cubo():
         area = 6 * (lado ** 2)
         return area
 
-    # Função para calcular o volume do cubo
+    # Calcular o volume do cubo
     def volume_cubo():
         volume = lado ** 3
         return volume
